61122f145b95e1bc7f06c43533e21303d0deb681
vladimirbahmetyev <vladimir240298perm@gmail.com>
2017-03-22 16:02:59 +0300

    Upload hw 1/5

---
+++ 1st_course/2nd_sem/HomeWork_1/Hw1_5/ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1_5
+{
+    public class Program
+    {
+        public static void SwapString(int[,] matrix, int firstString, int secondString, int sizeOfString)
+        {
+            for (int i = 0; i < sizeOfString; i++)
+            {
+                int timeNumber = matrix[firstString,i];
+                matrix[firstString, i] = matrix[secondString, i];
+                matrix[secondString, i] = timeNumber;
+            }
+        }
+        public static void MatrixSort(int[,] matrix, int sizeOfString, int countOfString)
+        {
+            const int firstElement = 0;
+            for (int i = 0; i < countOfString; i++)
+            {
+                int min = matrix[i,firstElement];
+                int minNumber = i;
+                for (int j = i + 1; j < countOfString; j++)
+                {
+                    if (matrix[j, firstElement] < min)
+                    {
+                        min = matrix[j, firstElement];
+                        minNumber = j;
+                    }
+                }
+                Program.SwapString(matrix, i, minNumber, sizeOfString);
+            }
+        }
+            static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 1st_course/2nd_sem/HomeWork_1/Hw1_5/Hw1_5Tests/ProgramTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hw1_5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1_5.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        const int sizeOfString = 2;
+        [TestMethod()]
+        public void SwapStringTest()
+        {
+            int[,] testMatrix = new int[sizeOfString,sizeOfString] { { 1,2}, { 3,4 } };
+            Program.SwapString(testMatrix, 0, 1, sizeOfString);
+            Assert.AreEqual(true, testMatrix[0, 0] == 3 && testMatrix[0, 1] == 4 && testMatrix[1, 0] == 1 && testMatrix[1, 1] == 2);
+        }
+
+        [TestMethod()]
+        public void MatrixSortTest()
+        {
+            int[,] testMatrix = new int[3, 2] { { 5, 2 }, { 3, 4 }, { 1,2 } };
+            Program.MatrixSort(testMatrix, sizeOfString, sizeOfString + 1);
+            bool flag = true;
+            for (int i = 0; i < sizeOfString; i++)
+            {
+                flag = flag && (testMatrix[i, 0] <= testMatrix[i + 1, 0]);
+            }
+            Assert.AreEqual(true, flag);
+        }
+    }
+}

0134f972596a6a541bbd3484eb9ca2fde8987f88
vladimirbahmetyev <vladimir240298perm@gmail.com>
2017-03-22 15:59:23 +0300

    UpLoad hw1/1

---
+++ 1st_course/2nd_sem/HomeWork_1/hw1_1/hw1_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw1_1
+{
+    public class Program
+    {
+        public static int Factorial(int n)
+        {
+            if(n < 1)
+            {
+                return -1;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}

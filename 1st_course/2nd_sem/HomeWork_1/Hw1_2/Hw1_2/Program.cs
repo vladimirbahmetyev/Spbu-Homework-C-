dd741b1460a8313ff0a5451bf35cd74a586e607a
vladimirbahmetyev <vladimir240298perm@gmail.com>
2017-03-22 16:00:20 +0300

    UpLoad hw1/2

---
+++ 1st_course/2nd_sem/HomeWork_1/Hw1_2/Hw1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1_2
+{
+    public class Program
+    {
+        public static int Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 1st_course/2nd_sem/HomeWork_1/Hw1_2/Hw1_2Tests/ProgramTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hw1_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw1_2.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void FibonacciTest1()
+        {
+            Assert.AreEqual(1, Program.Fibonacci(1));
+        }
+        [TestMethod()]
+        public void FibonacciTest2()
+        {
+            Assert.AreEqual(1, Program.Fibonacci(2));
+        }
+        [TestMethod()]
+        public void FibonacciTest3()
+        {
+            Assert.AreEqual(8, Program.Fibonacci(6));
+        }
+        [TestMethod()]
+        public void FibonacciTest4()
+        {
+            Assert.AreEqual(55, Program.Fibonacci(10));
+        }
+    }
+}

72d3a1fa36efd2f0d1afb58fe155ea243cc9da0e
vladimirbahmetyev <vladimir240298perm@gmail.com>
2017-04-05 13:46:34 +0300

    Correct mistakes and add Exception

--- 1st_course/2nd_sem/HomeWork_1/Hw1_2/Hw1_2/Program.cs
+++ 1st_course/2nd_sem/HomeWork_1/Hw1_2/Hw1_2/Program.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hw1_2
+namespace Hw12
 {
     public class Program
     {
+        /// <summary>
+        /// Calculating Fibonacci number
+        /// </summary>
+        /// <returns> Fibonacci number n </returns>
         public static int Fibonacci(int n)
         {
+            if (n < 1)
+            {
+                throw new InvalidOperationException();
+            }
             if (n == 1 || n == 2)
             {
                 return 1;
@@ -19,6 +27,7 @@ namespace Hw1_2
                 return Fibonacci(n - 1) + Fibonacci(n - 2);
             }
         }
+
         static void Main(string[] args)
         {
         }
--- 1st_course/2nd_sem/HomeWork_1/Hw1_2/Hw1_2Tests/ProgramTests.cs
+++ 1st_course/2nd_sem/HomeWork_1/Hw1_2/Hw1_2Tests/ProgramTests.cs
@@ -1,35 +1,45 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Hw1_2;
+using Hw12;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hw1_2.Tests
+namespace Hw12.Tests
 {
     [TestClass()]
     public class ProgramTests
     {
         [TestMethod()]
-        public void FibonacciTest1()
+        public void Fibonacci1ShouldBe1Test()
         {
             Assert.AreEqual(1, Program.Fibonacci(1));
         }
+
         [TestMethod()]
-        public void FibonacciTest2()
+        public void Fibonacci2ShouldBe1Test()
         {
             Assert.AreEqual(1, Program.Fibonacci(2));
         }
+
         [TestMethod()]
-        public void FibonacciTest3()
+        public void Fibonacci6ShouldBe8Test()
         {
             Assert.AreEqual(8, Program.Fibonacci(6));
         }
+
         [TestMethod()]
-        public void FibonacciTest4()
+        public void Fibonacci10ShouldBe55Test()
         {
             Assert.AreEqual(55, Program.Fibonacci(10));
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Fibonacci0ShouldBeExceptionTest()
+        {
+            Assert.AreEqual(0, Program.Fibonacci(0));
         }
     }
 }

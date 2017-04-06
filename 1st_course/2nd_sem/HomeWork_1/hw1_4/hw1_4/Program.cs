b7adb221f6de9ee2cc0a19ade426c8949060597e
vladimirbahmetyev <vladimir240298perm@gmail.com>
2017-04-06 14:19:43 +0300

    Correct Mistakes add Exception

--- 1st_course/2nd_sem/HomeWork_1/hw1_4/hw1_4/Program.cs
+++ 1st_course/2nd_sem/HomeWork_1/hw1_4/hw1_4/Program.cs
@@ -4,33 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hw1_4
+namespace Hw14
 {
-    class Program
+    public class Program
     {
-        static void PrintMatrix(int[,] matrix, int sizeOfMatrix)
+        /// <summary>
+        /// Transofm matrix to array
+        /// </summary>
+        /// <returns> Spiral array</returns>
+        public int[] TransformMatrix(int[,] matrix)
         {
+            int[] array = new int[matrix.Length];
+            int k = 1;
             bool horizontal = true;
             bool vertical = true;
-            int[] position = new int[2] { sizeOfMatrix / 2, sizeOfMatrix / 2 };
-            Console.Write("{0} ", matrix[position[0], position[1]]);
-            for (int i = 1; i <= sizeOfMatrix; i++)
+            int[] position = new int[2] { matrix.GetLength(0) / 2, matrix.GetLength(0) / 2 };
+            array[0] = matrix[position[0], position[1]];
+            for (int i = 1; i <= matrix.GetLength(0); i++)
             {
-                for (int j = 0; j <i; j++)
+                for (int j = 0; j < i; j++)
                 {
                     if (horizontal)
                     {
                         position[1]++;
-                        if (position[0] >= sizeOfMatrix || position[1] >= sizeOfMatrix)
+                        if (position[1] >= matrix.GetLength(0))
                         {
-                            return;
+                            return array;
                         }
-                        Console.Write("{0} ", matrix[position[0], position[1]]);
+                        array[k] = matrix[position[0], position[1]];
+                        k++;
                     }
                     else
                     {
+
                         position[1]--;
-                        Console.Write("{0} ", matrix[position[0], position[1]]);
+                        if (position[1] < 0)
+                        {
+                            return array;
+                        }
+                        array[k] = matrix[position[0], position[1]];
+                        k++;
                     }
                 }
                 for (int j = 0; j < i; j++)
@@ -38,29 +51,52 @@ namespace hw1_4
                     if (vertical)
                     {
                         position[0]++;
-
-                        if (position[0] >= sizeOfMatrix || position[1] >= sizeOfMatrix)
+                        if (position[0] >= matrix.GetLength(0))
                         {
-                            return;
+                            return array;
                         }
-                        Console.Write("{0} ", matrix[position[0], position[1]]);
+                        array[k] = matrix[position[0], position[1]];
+                        k++;
                     }
                     else
                     {
                         position[0]--;
-                        Console.Write("{0} ", matrix[position[0], position[1]]);
-                    }           
+                        if (position[0] < 0)
+                        {
+                            return array;
+                        }
+                        array[k] = matrix[position[0], position[1]];
+                        k++;
+                   }
                 }
                 horizontal = !horizontal;
                 vertical = !vertical;
             }
+            return array;
+        }
+
+        /// <summary>
+        /// Print array
+        /// </summary>
+        public void PrintArray (int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+        }
+
+        /// <summary>
+        /// Print Matrix from center to edge
+        /// </summary>
+        public void PrintMatrix(int[,] matrix)
+        {
+            int[] array = TransformMatrix(matrix);
+            PrintArray(array);
         }
 
         static void Main(string[] args)
-        {
-            int sizeOfMatrix = 3;
-            int[,] matrix = new int[3, 3] { {7, 8, 9},{6,1 ,2},{5, 4, 3} };
-            PrintMatrix(matrix, sizeOfMatrix);            
+        {        
         }
     }
 }

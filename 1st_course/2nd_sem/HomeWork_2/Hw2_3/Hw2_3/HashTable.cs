62bcfe6d013e502ae36d8ce889f183fe4062c8e7
vladimirbahmetyev <vladimir240298perm@gmail.com>
2017-05-05 14:08:51 +0300

    Make main functional, add unit tests, modified List.cs

---
+++ 1st_course/2nd_sem/HomeWork_2/Hw2_3/Hw2_3/HashTable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Hw2_3
+{
+    public class HashTable
+    {
+        private const int sizeOfHashTable = 10000;
+
+        private List[] hashTableElements;
+        public HashTable()
+        {
+            hashTableElements = new List[sizeOfHashTable];
+
+            for (var i = 0; i < sizeOfHashTable; ++i)
+            {
+                hashTableElements[i] = new List();
+            }
+        }
+
+        private int HashFunction(string inputString)
+        {
+            int hash = 0;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                hash = hash + inputString[i] * i + 1701;
+            }
+            return Math.Abs(hash) % sizeOfHashTable;
+        }
+
+        public bool IsElementInHashTable(string inputString)
+        {
+            return hashTableElements[HashFunction(inputString)].IsElementInList(inputString);
+        }
+
+        public void AddNewElementToHashTable(string inputString)
+        {
+            if (!IsElementInHashTable(inputString))
+            {
+                hashTableElements[HashFunction(inputString)].AddValueToPosition(inputString, 0);
+            }
+        }
+
+        public void DeleteElementFromHashTable (string deleteString)
+        {
+            if (IsElementInHashTable(deleteString))
+            {
+                hashTableElements[HashFunction(deleteString)].DeleteElementFromList(deleteString);
+            }
+        }
+
+    }
+}
---
+++ 1st_course/2nd_sem/HomeWork_2/Hw2_3/Hw2_3Tests1/HashTableTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hw2_3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hw2_3.Tests
+{
+    [TestClass()]
+    public class HashTableTests
+    {
+        private HashTable testHashTable;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            testHashTable = new HashTable();
+        }
+
+        [TestMethod()]
+        public void IsElementInHashTableTest()
+        {
+            testHashTable.AddNewElementToHashTable("Hello");
+            Assert.IsTrue(testHashTable.IsElementInHashTable("Hello"));
+        }
+
+        [TestMethod()]
+        public void AddNewElementToHashTableTest()
+        {
+            testHashTable.AddNewElementToHashTable("Hello");
+            testHashTable.AddNewElementToHashTable("World");
+            testHashTable.AddNewElementToHashTable("!");
+            Assert.IsTrue(testHashTable.IsElementInHashTable("Hello") && testHashTable.IsElementInHashTable("World") && testHashTable.IsElementInHashTable("!"));
+        }
+
+        [TestMethod()]
+        public void DeleteElementFromHashTableTest()
+        {
+            testHashTable.AddNewElementToHashTable("Hello");
+            testHashTable.DeleteElementFromHashTable("Hello");
+            Assert.IsTrue(!testHashTable.IsElementInHashTable("Hello"));
+        }
+    }
+}

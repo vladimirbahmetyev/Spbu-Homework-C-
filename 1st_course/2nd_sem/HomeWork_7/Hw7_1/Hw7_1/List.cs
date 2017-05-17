c151d5b45c782f3cd601609f9740290ddc1c2d98
vladimirbahmetyev <vladimir240298perm@gmail.com>
2017-05-17 23:31:25 +0300

    Modified List.cs and Stack.cs and correct UnitTests

---
+++ 1st_course/2nd_sem/HomeWork_7/Hw7_1/Hw7_1/List.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Hw71
+{
+    /// <summary>
+    /// Класс связный список
+    /// </summary>
+    public class List<T> : IEnumerable
+    {
+        /// <summary>
+        /// Элемент связного списка
+        /// </summary>
+        private class ListElement
+        {
+            /// <summary>
+            /// Значение элемента списка
+            /// </summary>
+            public T Value { get; set; }
+
+            /// <summary>
+            /// Указатель на следующий элемент списка
+            /// </summary>
+            public ListElement Next { get; set; }
+
+            /// <summary>
+            /// Конструктор для класса ListElement
+            /// </summary>
+            /// <param name="Value">Значение элемента</param>
+            /// <param name="Next">указатель на следующий элемент</param>
+            public ListElement(T value, ListElement next)
+            {
+                Value = value;
+                Next = next;
+            }
+        }
+
+        /// <summary>
+        /// Указатель на первый элемент списка
+        /// </summary>
+        private ListElement head;
+
+        /// <summary>
+        /// Длина списка
+        /// </summary>
+        private int length;
+
+        /// <summary>
+        /// Добавление элемента на n-ую корректную позицию\
+        /// n >= 0
+        /// position не может быть больше length
+        /// </summary>
+        /// <param name="Value">Значение нового элемента</param>
+        /// <param name="position">Позиция нового элемента</param>
+        public void AddValueToPosition(T Value, int position)
+        {
+            if (position < 0 || position > length)
+            {
+                throw new InvalidOperationException("Incorrect position!");
+            }
+            if (position == 0)
+            {
+                head = new ListElement(Value, head);
+                length++;
+                return;
+            }
+            ListElement cursor = head;
+            for (var i = 0; i < position - 1; i++)
+            {
+                cursor = cursor.Next;               
+            }
+            var newElement = new ListElement(Value, cursor.Next);
+            cursor.Next = newElement;
+            length++;
+        }
+
+        /// <summary>
+        /// Получение значения n-ого элемента
+        /// n >= 0
+        /// position не может быть больше length
+        /// </summary>
+        /// <param name="position">Позиция искомого элемента</param>
+        /// <returns></returns>
+        public T GetValueByPosition(int position)
+        {
+            if (position < 0 || position >= length)
+            {
+                throw new InvalidOperationException("Incorrect position!");
+            }
+
+            var cursor = head;
+            for (var i = 0; i < position; ++i)
+            {
+                cursor = cursor.Next;
+            }
+
+            return cursor.Value;
+        }
+
+        /// <summary>
+        /// Смена значения элемента на n-ой позиции на новое
+        /// n >= 0
+        /// position не может быть больше length
+        /// </summary>
+        /// <param name="Value">Новое значение</param>
+        /// <param name="position">Позиция заменяемого элемента</param>
+        public void ChangeValueByPosition(T Value, int position)
+        {
+            if (position < 0 || position >= length)
+            {
+                throw new InvalidOperationException("Incorrect position!");
+            }
+            ListElement cursor = head;
+            for (int i = 0; i < position; i++)
+            {
+                cursor = cursor.Next;
+            }
+            cursor.Value = Value;
+        }
+
+        /// <summary>
+        /// Удаление элемента с n-ой позиции
+        /// n >= 0
+        /// position не может быть больше length
+        /// </summary>
+        /// <param name="position">Позиция элемента, который нужно удалить</param>
+        public void RemoveElementFromPosition(int position)
+        {
+            if (position < 0 || position >= length)
+            {
+                throw new InvalidOperationException("Incorrect position!");
+            }
+            if (position == 0)
+            {
+                head = head.Next;
+                length--;
+                return;
+            }
+            ListElement cursor = head;
+            for (int i = 0; i < position - 1; i++)
+            {
+                cursor = cursor.Next;
+            }
+            cursor.Next = cursor.Next.Next;
+            length--;
+        }
+
+        /// <summary>
+        /// Вовзращает длину списка
+        /// </summary>
+        /// <returns></returns>
+        public int GetLength() => length;
+
+        /// <summary>
+        /// Генериковый перечислитель
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<T> GetEnumerator() => new ListEnumerator(this);
+
+        /// <summary>
+        /// Негенериковый перечислитель
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator IEnumerable.GetEnumerator() => new ListEnumerator(this);
+
+        /// <summary>
+        /// Класс для перебора коллекции
+        /// </summary>
+        private class ListEnumerator : IEnumerator<T>
+        {
+            /// <summary>
+            /// Указатель на первый элемент
+            /// </summary>
+            private ListElement head;
+
+            /// <summary>
+            /// Указатель на текущий элемент
+            /// </summary>
+            private ListElement cursor;
+
+            /// <summary>
+            /// Конструктор перечислителя
+            /// </summary>
+            public ListEnumerator(List<T> list)
+            {
+                head = list.head;
+            }
+
+            /// <summary>
+            /// Возвращает значение текущего элемента
+            /// </summary>
+            public T Current
+            {
+                get
+                {
+                    return cursor.Value;
+                }
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return Current;
+                }
+            }
+
+            public void Dispose()
+            {
+
+            }
+
+            /// <summary>
+            /// Переход к следующему элементу
+            /// </summary>
+            public bool MoveNext()
+            {
+                if (head == null)
+                {
+                    return false;
+                }
+                if (cursor == null)
+                {
+                    cursor = head;               
+
+                    return true;
+                }
+
+                if (cursor.Next == null)
+                {
+                    return false;
+                }
+
+                cursor = cursor.Next;
+                return true;
+            }
+            
+            /// <summary>
+            /// Сдвинуть указатель на начальную позицию
+            /// </summary>
+            public void Reset() => cursor = null;
+        }
+    }
+}
---
+++ 1st_course/2nd_sem/HomeWork_7/Hw7_1/Hw7_1Tests/ListTests.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Hw71.Tests
+{
+    [TestClass()]
+    public class ListTests
+    {
+        private List<int> list;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            list = new List<int>();
+        }
+
+        [TestMethod()]
+        public void AddFiveToSecondPositionTest()
+        {
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(5, 2);
+            Assert.AreEqual(list.GetValueByPosition(2), 5);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void AddFiveToEmptyListOnSecondPositionExceptionTest()
+        {
+            list.AddValueToPosition(5, 2);
+        }
+
+        [TestMethod()]
+        public void GetValueByZeroPositionTest()
+        {
+            list.AddValueToPosition(1, 0);
+            Assert.AreEqual(1, list.GetValueByPosition(0));
+        }
+
+        [TestMethod()]
+        public void ChangeFiveBySecondPositionTest()
+        {
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(5, 2);
+            list.ChangeValueByPosition(1, 2);
+            bool flag = true;
+            for (int i = 0; i < list.GetLength(); i++)
+            {
+                flag = flag && (1 == list.GetValueByPosition(i));
+            }
+            Assert.IsTrue(flag);
+        }
+
+        [TestMethod()]
+        public void RemoveFiveFromListTest()
+        {
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(1, 0);
+            list.AddValueToPosition(5, 2);
+            list.RemoveElementFromPosition(2);
+            for (int i = 0; i < list.GetLength(); i++)
+            {
+                Assert.AreEqual(1, list.GetValueByPosition(i));
+            }
+        }
+
+        [TestMethod]
+        public void EnumeratorForEmptyListTest()
+        {
+            foreach (var element in list)
+            {
+
+            }
+        }
+
+        [TestMethod]
+        public void EnumeratorForListOfOneElementTest()
+        {
+            list.AddValueToPosition(1, 0);
+            foreach (var element in list)
+            {
+                Assert.AreEqual(1, element);
+            }
+        }
+
+        [TestMethod]
+        public void EnumeratorForListOfFiftyElementsTest()
+        {
+            for (int i = 0; i <= 50; i++)
+            {
+                list.AddValueToPosition(50 - i, i);
+            }
+
+            int value = 50;
+
+            foreach (int element in list)
+            {
+                Assert.AreEqual(element, value);
+                value--;
+            }
+        }
+    }
+}

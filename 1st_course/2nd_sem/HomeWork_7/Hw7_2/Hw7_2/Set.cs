86f223c220d9afcd72d6df48b9f964864300caf9
vladimirbahmetyev <vladimir240298perm@gmail.com>
2017-05-21 15:34:24 +0300

    Correct and add tests

--- 1st_course/2nd_sem/HomeWork_7/Hw7_2/Hw7_2/Set.cs
+++ 1st_course/2nd_sem/HomeWork_7/Hw7_2/Hw7_2/Set.cs
@@ -6,71 +6,67 @@
     public class Set<T>
     {
         /// <summary>
-        /// Класс операции над множествами
+        /// Объединение множеств
         /// </summary>
-        public class SetOperations
+        public static Set<T> UnionSet(Set<T> setOne, Set<T> setTwo)
         {
-            /// <summary>
-            /// Объединение множеств
-            /// </summary>
-            public static Set<T> UnionSet(Set<T> setOne, Set<T> setTwo)
+            Set<T> newSet = new Set<T>();
+            foreach (T element in setOne.List)
             {
-                Set<T> newSet = new Set<T>();
-                foreach (T element in setOne.List)
+                newSet.List.AddValueToPosition(element, 0);
+            }
+            foreach (T element in setTwo.List)
+            {
+                if (!newSet.List.IsContained(element))
+                {
+                    newSet.AddValue(element);
+                }
+            }
+            return newSet;
+        }
+
+        /// <summary>
+        /// Пересечение множеств
+        /// </summary>
+        public static Set<T> Intersection(Set<T> setOne, Set<T> setTwo)
+        {
+            Set<T> newSet = new Set<T>();
+            foreach (T element in setOne.List)
+            {
+                if (setTwo.List.IsContained(element))
                 {
                     newSet.List.AddValueToPosition(element, 0);
                 }
-                foreach (T element in setTwo.List)
-                {
-                    if (!newSet.List.IsContained(element))
-                    {
-                        newSet.List.AddValueToPosition(element, 0);
-                    }
-                }
-                return newSet;
             }
-
-            /// <summary>
-            /// Пересечение множеств
-            /// </summary>
-            public static  Set<T> Intersection(Set<T> setOne, Set<T> setTwo)
-            {
-                Set<T> newSet = new Set<T>();
-                foreach (T element in setOne.List)
-                {
-                    if (setTwo.List.IsContained(element))
-                    {
-                        newSet.List.AddValueToPosition(element, 0);
-                    }
-                }
-                return newSet;
-            }
-
-            /// <summary>
-            /// Эквивалентность множеств
-            /// </summary>
-            public static bool AreEqual(Set<T> setOne, Set<T> setTwo)
-            {
-                foreach (T element in setOne.List)
-                {
-                    if (!setTwo.List.IsContained(element))
-                    {
-                        return false;
-                    }
-                }
-                return true;
-            }
+            return newSet;
         }
+
+        /// <summary>
+        /// Эквивалентность множеств
+        /// </summary>
+        public static bool AreEqual(Set<T> setOne, Set<T> setTwo)
+        {
+            foreach (T element in setOne.List)
+            {
+                if (!setTwo.List.IsContained(element))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
 
         /// <summary>
         /// Операция по добавлению нового элемента в множество
         /// </summary>
         /// <param name="value"></param>
-        public void AddValueToSet(T value)
+        public void AddValue(T value)
         {
             if (!List.IsContained(value))
             {
-               List. AddValueToPosition(value, 0);
+               List.AddValueToPosition(value, 0);
+                size++;
             }
         }
 
@@ -82,12 +78,15 @@
         /// <summary>
         /// Удаляет элемент из множества
         /// </summary>
-        public void RemoveElementFromSet(T value) => List.DeleteElementFromList(value);
-
+        public void RemoveElementFromSet(T value)
+        {
+            List.DeleteElementFromList(value);
+            size--;
+        }
         /// <summary>
         /// Голова множества
         /// </summary>
-        private List<T> List { get; set; }
+        List<T> List { get; set; }
 
         /// <summary>
         /// конструктор множества
@@ -96,5 +95,17 @@
         {
             List = new List<T>();
         }
+
+        /// <summary>
+        /// Кол-во элементов в множестве
+        /// </summary>
+        private int size = 0;
+
+        /// <summary>
+        /// Выводит кол-во элементов в множестве
+        /// </summary>
+        /// <returns></returns>
+        public int GetSize() => size;
+
     }
 }
--- 1st_course/2nd_sem/HomeWork_7/Hw7_2/Hw7_2Tests/SetTests.cs
+++ 1st_course/2nd_sem/HomeWork_7/Hw7_2/Hw7_2Tests/SetTests.cs
@@ -18,27 +18,27 @@ namespace Hw72.Tests
         }
 
         [TestMethod()]
-        public void AddValueToSetTest()
+        public void AddValueTest()
         {
-            set.AddValueToSet(1);
+            set.AddValue(1);
             Assert.IsTrue(set.IsElementInSet(1));
         }
 
         [TestMethod()]
         public void IsElementInSetTest()
         {
-            set.AddValueToSet(1);
-            set.AddValueToSet(2);
-            set.AddValueToSet(3);
-            Assert.AreEqual(true, set.IsElementInSet(2));
+            set.AddValue(1);
+            set.AddValue(2);
+            set.AddValue(3);
+            Assert.IsTrue(set.IsElementInSet(2));
         }
 
         [TestMethod()]
         public void removeElementFromSetTest()
         {
-            set.AddValueToSet(1);
-            set.AddValueToSet(2);
-            set.AddValueToSet(3);
+            set.AddValue(1);
+            set.AddValue(2);
+            set.AddValue(3);
             set.RemoveElementFromSet(2);
             Assert.IsFalse(set.IsElementInSet(2));
         }
@@ -46,13 +46,13 @@ namespace Hw72.Tests
         [TestMethod()]
         public void UnionTest()
         {
-            firstSet.AddValueToSet(1);
-            firstSet.AddValueToSet(2);
-            firstSet.AddValueToSet(3);
-            secondSet.AddValueToSet(4);
-            secondSet.AddValueToSet(5);
-            secondSet.AddValueToSet(6);
-            var resultSet = Set<int>.SetOperations.UnionSet(firstSet, secondSet);
+            firstSet.AddValue(1);
+            firstSet.AddValue(2);
+            firstSet.AddValue(3);
+            secondSet.AddValue(4);
+            secondSet.AddValue(5);
+            secondSet.AddValue(6);
+            var resultSet = Set<int>.UnionSet(firstSet, secondSet);
             for (int i = 1; i < 7; i++)
             {
                 Assert.IsTrue(resultSet.IsElementInSet(i));
@@ -62,13 +62,13 @@ namespace Hw72.Tests
         [TestMethod()]
         public void IntersectionTest()
         {
-            firstSet.AddValueToSet(1);
-            firstSet.AddValueToSet(2);
-            firstSet.AddValueToSet(3);
-            secondSet.AddValueToSet(2);
-            secondSet.AddValueToSet(3);
-            secondSet.AddValueToSet(4);
-            var resultSet = Set<int>.SetOperations.Intersection(firstSet, secondSet);
+            firstSet.AddValue(1);
+            firstSet.AddValue(2);
+            firstSet.AddValue(3);
+            secondSet.AddValue(2);
+            secondSet.AddValue(3);
+            secondSet.AddValue(4);
+            var resultSet = Set<int>.Intersection(firstSet, secondSet);
             Assert.IsTrue(resultSet.IsElementInSet(2));
             Assert.IsTrue(resultSet.IsElementInSet(3));
             Assert.IsFalse(resultSet.IsElementInSet(1));
@@ -78,13 +78,21 @@ namespace Hw72.Tests
         [TestMethod]
         public void EqualsTest()
         {
-            firstSet.AddValueToSet(1);
-            firstSet.AddValueToSet(2);
-            firstSet.AddValueToSet(3);
-            secondSet.AddValueToSet(1);
-            secondSet.AddValueToSet(2);
-            secondSet.AddValueToSet(3);
-            Assert.IsTrue(Set<int>.SetOperations.AreEqual(firstSet, secondSet));
+            firstSet.AddValue(1);
+            firstSet.AddValue(2);
+            firstSet.AddValue(3);
+            secondSet.AddValue(1);
+            secondSet.AddValue(2);
+            secondSet.AddValue(3);
+            Assert.IsTrue(Set<int>.AreEqual(firstSet, secondSet));
+        }
+
+        [TestMethod]
+        public void AddOneElementToSetTwiceTest()
+        {
+            firstSet.AddValue(1);
+            firstSet.AddValue(1);
+            Assert.AreEqual(1, firstSet.GetSize());
         }
     }
 }

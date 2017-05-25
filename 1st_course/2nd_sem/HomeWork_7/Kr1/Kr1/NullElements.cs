6ab10fa4ef0593dfae52cb487b484855b069ac19
vladimirbahmetyev <vladimir240298perm@gmail.com>
2017-05-25 17:48:53 +0300

    Kontrol work 1

---
+++ 1st_course/2nd_sem/HomeWork_7/Kr1/Kr1/NullElements.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kr1
+{
+    /// <summary>
+    /// Класс списка нулевых элементов
+    /// </summary>
+    public class NullElementList
+    {
+        /// <summary>
+        /// Нулевые элементы по умолчанию
+        /// </summary>
+        private int Int = 0;
+        private double Double = -1;
+        private string String = "";
+        private bool Bool = false;
+        private char Char = '!';
+
+        /// <summary>
+        /// Возвращает нулевые элементы для каждого типа
+        /// </summary>
+        /// <param name="inputObject">Пример обьекта</param>
+        /// <returns>Ноль для типа этого обьекта</returns>
+        /// Для типов, которые тут не написаны делается аналогично
+        public object GetNullForThisType(object inputObject)
+        {
+            if(inputObject is int)
+            {
+                return Int;
+            }
+            if (inputObject is bool)
+            {
+                return Bool;
+            }
+            if (inputObject is string)
+            {
+                return String;
+            }
+            if (inputObject is double)
+            {
+                return Double;
+            }
+            if (inputObject is char)
+            {
+                return Char;
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Устанавливает новые нулевые элементы
+        /// </summary>
+        public void SetNulls(int newInt, double newDouble, string newString, bool newBool, char newChar)
+        {
+            Int = newInt;
+            Double = newDouble;
+            String = newString;
+            Bool = newBool;
+            Char = newChar;
+        }
+    }
+}
---
+++ 1st_course/2nd_sem/HomeWork_7/Kr1/Kr1Tests/ChekingForNullElementTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kr1.Tests
+{
+    [TestClass()]
+    public class ChekingForNullElementTests
+    {
+        private List<int> listInt;
+        private List<string> listString;
+        private NullElementList newList;
+        private List<bool> listBool;
+        private List<double> listDouble;
+        private List<char> listChar;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            listInt = new List<int>();
+            newList = new NullElementList();
+            listString = new List<string>();
+            listBool = new List<bool>();
+            listDouble = new List<double>();
+            listChar = new List<char>();
+        }
+
+        [TestMethod()]
+        public void CountNullElementsTestInt()
+        {
+            listInt.AddValueToPosition(1, 0);
+            listInt.AddValueToPosition(1, 0);
+            listInt.AddValueToPosition(0, 0);
+            Assert.AreEqual(1, listInt.CountNullElements(newList));
+        }
+
+        [TestMethod()]
+        public void CountNullElementsTestString()
+        {
+            listString.AddValueToPosition("Hello", 0);
+            listString.AddValueToPosition("World", 0);
+            listString.AddValueToPosition("!", 0);
+            listString.AddValueToPosition("", 0);
+            Assert.AreEqual(1, listString.CountNullElements(newList));
+        }
+
+        [TestMethod()]
+        public void CountNullElementsTestBool()
+        {
+            listBool.AddValueToPosition(true, 0);
+            listBool.AddValueToPosition(false, 0);
+            listBool.AddValueToPosition(true, 0);
+            listBool.AddValueToPosition(false, 0);
+            Assert.AreEqual(2, listBool.CountNullElements(newList));
+        }
+
+        [TestMethod()]
+        public void CountNullElementsTestDouble()
+        {
+            listDouble.AddValueToPosition(-1, 0);
+            listDouble.AddValueToPosition(1, 0);
+            listDouble.AddValueToPosition(-1, 0);
+            listDouble.AddValueToPosition(1, 0);
+            Assert.AreEqual(2, listDouble.CountNullElements(newList));
+        }
+
+        [TestMethod()]
+        public void CountNullElementsTestDoubleEmpty()
+        {
+            Assert.AreEqual(0, listDouble.CountNullElements(newList));
+        }
+
+        [TestMethod()]
+        public void CountNullElementsTestChar()
+        {
+            listChar.AddValueToPosition('H', 0);
+            listChar.AddValueToPosition('e', 0);
+            listChar.AddValueToPosition('l', 0);
+            listChar.AddValueToPosition('l', 0);
+            listChar.AddValueToPosition('o', 0);
+            listChar.AddValueToPosition('w', 0);
+            listChar.AddValueToPosition(' ', 0);
+            listChar.AddValueToPosition('W', 0);
+            listChar.AddValueToPosition('o', 0);
+            listChar.AddValueToPosition('r', 0);
+            listChar.AddValueToPosition('l', 0);
+            listChar.AddValueToPosition('d', 0);
+            listChar.AddValueToPosition('!', 0);
+            listChar.AddValueToPosition('!', 0);
+            listChar.AddValueToPosition('!', 0);
+            Assert.AreEqual(3, listChar.CountNullElements(newList));
+        }
+    }
+}
